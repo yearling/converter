d0ceb29673dda5a11edf3028a37f02970a2ed4c9
yearling <yearling@163.com>
2022-03-06 02:57:20 +0800

    fix bug

--- engine/src/Engine/TaskGraph.cpp
+++ engine/src/Engine/TaskGraph.cpp
@@ -1506,7 +1506,6 @@ void FGraphEvent::DispatchSubsequents(std::vector<FBaseGraphTask*>& NewTasks, EN
 	}
 
 	SubsequentList.PopAllAndClose(NewTasks);
-	for (size_t Index = NewTasks.size() - 1; Index >= 0; Index--) // reverse the order since PopAll is implicitly backwards
 	for (int Index = (int)(NewTasks.size()) - 1; Index >= 0; Index--) // reverse the order since PopAll is implicitly backwards
 	{
 		FBaseGraphTask* NewTask = NewTasks[Index];
